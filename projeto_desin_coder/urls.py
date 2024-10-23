6ce25022bc15c7fc4ed4391221a28371b5bea410
DanielDemico <135667661+DanielDemico@users.noreply.github.com>
2024-10-23 20:09:00 -0300

    Models e Views arrumados

--- projeto_desin_coder/urls.py
+++ projeto_desin_coder/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
     path('naves/',views.naves,name='naves'),
     path('patos/',views.patos,name='patos')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
