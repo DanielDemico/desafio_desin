1fac27aa6272a3371afc307b348cbfbc3ad433f7
DanielDemico <135667661+DanielDemico@users.noreply.github.com>
2024-10-27 17:14:34 -0300

    finalizando

--- app/views.py
+++ app/views.py
@@ -11,7 +11,7 @@ def naves(request):
     naves_front = {
         'naves': Nave_Certa.objects.all()
     }
-   
+    #Nave_Certa.objects.all().delete() Para deletar todos os registros
     
     return render(request,'naves.html',naves_front)
 
